5be13e4ec9809fe8abc07afdd7cbc1c7b53bd647
Donaldo <donaldo.jinette@gmail.com>
2021-03-31 18:52:24 -0500

    Small changes

--- rino/urls.py
+++ rino/urls.py
@@ -36,5 +36,6 @@ urlpatterns = [
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # to import static in deployment
 
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
