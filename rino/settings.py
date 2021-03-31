25d6fdfd5cb9a7cf581ec26a6967c991b5b84385
Miguel Cubides <mcubides@gmail.com>
2021-03-31 18:05:11 -0500

    *

--- rino/settings.py
+++ rino/settings.py
@@ -142,8 +142,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "static/correspondence/thumbnails")
+    os.path.join(BASE_DIR, "static")
 ]
 
 MEDIA_ROOT = MEDIA_DIR
