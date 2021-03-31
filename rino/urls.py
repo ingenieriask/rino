7ab488f45ee6143e7de7fa4da0d84ffcf4fec5a1
Donaldo <donaldo.jinette@gmail.com>
2021-03-31 18:50:07 -0500

    Small changes

--- rino/urls.py
+++ rino/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # to import static in deployment
 
 
 if settings.DEBUG:
